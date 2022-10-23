f8d6f8d67888095d106cb37a8331579f78071109
Anjeza <tun54437@temple.edu>
2022-10-23 14:45:09 -0400

    first try with login

--- face_recognition/src/App.js
+++ face_recognition/src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import {Fragment} from "react";
 import Navbar from "./component/Navbar"
 import {Button} from "@mui/material";
+import Attendance from "./Attendance";
+import {BrowserRouter as Router, Routes, Link, Route} from "react-router-dom";
+import * as PropTypes from "prop-types";
+
+
+
 
 function App() {
   return (
@@ -14,9 +21,15 @@ function App() {
             <p>
               Click on the button below to record your attendance
             </p>
-              <Button variant="contained">
-                  Record Attendance
-              </Button>
+
+              <Router>
+                  <Link to ="/attendance">Take Attendance</Link>
+                  <Routes>
+
+                      <Route exact path='/attendance' element ={<Attendance/>}></Route>
+
+                  </Routes>
+              </Router>
           </header>
         </div>
       </Fragment>
---
+++ face_recognition/src/Attendance.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Attendance.css';
+import {Button} from "@mui/material";
+function Attendance(){
+    return (
+        <div>
+        <p>
+            <Button>Report</Button>
+        </p>
+        </div>
+
+
+);
+
+
+}
+export default Attendance;
